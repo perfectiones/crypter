ef5309580fcb3669d65c49fed5ecc6b70eefd748
dmitriy oginov <dmitrij.oginov@mail.ru>
2023-03-30 22:14:00 +0300

    crateProject

---
+++ src/App.js
@@ -0,0 +1,20 @@
+//import { Routes , Route } from 'react-router-dom';
+import "./styles/bootstrap.min.css";
+import "./styles/null.css";
+
+import Header from "./components/Header";
+
+function App() {
+  
+  return (
+      <>
+        <Header />
+      </>
+
+  );
+  
+}
+
+export default App;
+
+
